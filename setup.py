d5a6a5c7b2b15772dfe44b72366ed0ae77de4578
BlakeeBobz <123bobisgaming@gmail.com>
2025-07-30 09:36:22 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pythonsavedata',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # list dependencies here, e.g. 'requests',
+    ],
+    author='BlakeeBobz',
+    description='Simple way to save data in python',
+    url='https://github.com/BlakeeBobz/PythonSaveData',
+)
